4125540b56f74dfbfdb632ebf4f96d6a176b16ad
Leandro Neves <leandrosoares6@hotmail.com>
2021-07-28 08:39:23 +0000

    validation in controller and returning status created with location uri

--- src/main/java/br/com/leandro/todolist/adapters/api/controllers/AddTodoController.java
+++ src/main/java/br/com/leandro/todolist/adapters/api/controllers/AddTodoController.java
@@ -1,11 +1,16 @@
 package br.com.leandro.todolist.adapters.api.controllers;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.leandro.todolist.adapters.api.requests.TodoRequest;
 import br.com.leandro.todolist.adapters.api.responses.TodoResponse;
@@ -24,10 +29,14 @@ public class AddTodoController {
 	private AddTodoOutput viewModel;
 
 	@PostMapping
-	public ResponseEntity<TodoResponse> AddTodo(@RequestBody TodoRequest todoRequest) {
+	public ResponseEntity<TodoResponse> AddTodo(@Valid @RequestBody TodoRequest todoRequest) {
 		AddTodo addTodo = new AddTodo(todoRepository, viewModel);
 		TodoResponse response = (TodoResponse) addTodo.execute(TodoRequest.convertToTodoData(todoRequest));
-		return ResponseEntity.ok(response);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(response.getId())
+				.toUri();
+
+		return ResponseEntity.created(uri).build();
 	}
 
 }
